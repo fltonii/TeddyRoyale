253e4518afa7db640fd36757d4323add4f64fa49
Joao Tonial <joao.tonial@beehiveclub.com.br>
2018-10-08 20:50:18 -0300

    Adding game over

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -6,11 +6,18 @@ public class EnemyManager : MonoBehaviour
     public GameObject enemy;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
+    float timer = 0f;
+    void FixedUpdate() {
+        timer += Time.fixedDeltaTime;
 
+        if(timer >= spawnTime){
+            Spawn();
+            timer = 0f;
+        }
 
-    void Start ()
-    {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        if(ScoreManager.score % 100 == 0) {
+            spawnTime -= 2.5f; 
+        }
     }
 
     void Spawn ()
@@ -24,4 +31,6 @@ public class EnemyManager : MonoBehaviour
 
         Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
+
+
 }
